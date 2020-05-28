9abf330d1357a98194de76bbee1a4df14c507229
dalton-x <daltonx43@gmail.com>
2020-05-28 12:56:30 +0200

    Reprise de la methode create

--- Back-end/controllers/user.controller.js
+++ Back-end/controllers/user.controller.js
@@ -1,5 +1,6 @@
 // Ajout de plugin externe
 const bcrypt = require('bcrypt')        // sert a cryter une chaine de caractere
+const jwt = require('jsonwebtoken');    // sert a generer un token d'authentification
 const db = require("../models");
 const User = db.user;
 const Op = db.Sequelize.Op;
@@ -16,36 +17,36 @@ exports.create = (req, res) => {
     return;
   }
 
-    // Haschage du mot de passe
-    bcrypt.hash(req.body.password, 10)
-    .then(hash => {
-    
-    // Création de l'objet user avec le retour du front-end
-    const user = {
-        email: req.body.email,
-        firstname: req.body.firstname,
-        lastname: req.body.lastname,
-        password: hash,
-        pseudo: req.body.pseudo,
-        imageUrl: "http://localhost:3000/images/user.png",
-        online: 1,
-        isAdmin: 0,
-        dateInscription: new Date()
-    };
+  // Haschage du mot de passe
+  bcrypt.hash(req.body.password, 10)
+  .then(hash => {
   
-        // Création de l'utilisateur dans la BDD
-        User.create(user)
-            .then(data => {
-                res.send(data);
-            })
-            .catch(err => {
-                res.status(501).send({
-                message:
-                err.message && "Une erreur s'est produite lors de la création de l'utilisateur."
-            });
-        });
-    })
-    .catch(error => res.status(502).json({ error }));
+  // Création de l'objet user avec le retour du front-end
+  const user = {
+    email: req.body.email,
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+    password: hash,
+    pseudo: req.body.pseudo,
+    imageUrl: "http://localhost:3000/images/user.png",
+    online: 1,
+    isAdmin: 0,
+    dateInscription: new Date()
+  };
+  
+    // Création de l'utilisateur dans la BDD
+    User.create(user)
+      .then(data => {
+        res.send(data);
+      })
+      .catch(err => {
+        res.status(501).send({
+        message:
+        err.message && "Une erreur s'est produite lors de la création de l'utilisateur."
+      });
+    });
+  })
+  .catch(error => res.status(502).json({ error }));
 };
 
 // log un user et avec la verification de son mot de passe
