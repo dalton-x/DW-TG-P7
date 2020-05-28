e102400b1ef7e5827050408c84b2ff9c019aca46
dalton-x <daltonx43@gmail.com>
2020-05-28 11:14:04 +0200

    Mise en place du back end de base

---
+++ Back-end/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use(bodyParser.json());
+
+module.exports = app;
