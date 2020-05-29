6fea4fa077e2f7236ea877d5c4a024a79b4f3cd9
dalton-x <daltonx43@gmail.com>
2020-05-29 09:59:30 +0200

    modification des routes api

--- Back-end/routes/user.routes.js
+++ Back-end/routes/user.routes.js
@@ -10,10 +10,10 @@ module.exports = app => {
   router.post("/signup", userCtrl.create);
   router.post("/login", userCtrl.logIn);
   router.get("/",auth, userCtrl.getAll);
-  router.get("/:email",auth, multer, userCtrl.getOne);
+  router.get("/:id",auth, multer, userCtrl.getOne);
   router.get("/online",auth, userCtrl.getAllOnline);
-  router.put("/:email",auth, multer, userCtrl.update);
-  router.delete("/:email",auth, multer, userCtrl.delete);
+  router.put("/:id",auth, multer, userCtrl.update);
+  router.delete("/:id",auth, multer, userCtrl.delete);
 
   app.use('/api/user', router);
 
