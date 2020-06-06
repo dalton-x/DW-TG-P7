5adb93b0d483655b011c7755002170caae764ea0
dalton-x <daltonx43@gmail.com>
2020-06-06 10:39:32 +0200

    Passage sur 1 seul multer-config

--- Back-end/routes/post.routes.js
+++ Back-end/routes/post.routes.js
@@ -1,17 +1,18 @@
 // Ajout des middleweares
 const auth = require('../middleware/auth');
-const multer = require('../middleware/multer-config-post');
+const multer = require('../middleware/multer-config');
 
 module.exports = app => {
   const postCtrl = require("../controllers/post.controller.js");
 
   var router = require("express").Router();
 
-  router.post("/create/:id",auth, multer, postCtrl.createPost);     // Création d'un post par un utilisateur
-  router.get("/",auth, postCtrl.getAllPost);                        // Récuperer tout les posts crées
-  router.get("/:id",auth, postCtrl.getOnePost);                     // Récupere un pot en fonction de son Id
-  router.put("/:id",auth, multer, postCtrl.updatePost);             // Mettre a jour un post
-  router.delete("/:id",auth, multer, postCtrl.deletePost);          // Supprimer un post
+  router.post("/create/:id",auth, multer, postCtrl.createPost);         // Création d'un post par un utilisateur
+  router.get("/:keywords",auth, multer, postCtrl.getPostByKeywords);    // Récupération des mots clés     
+  router.get("/",auth, multer, postCtrl.getAllPost);                    // Récuperer tout les posts crées
+  router.get("/:id",auth, multer, postCtrl.getOnePost);                 // Récupere un pot en fonction de son Id
+  router.put("/:id",auth, multer, postCtrl.updatePost);                 // Mettre a jour un post
+  router.delete("/:id/:postId",auth, multer, postCtrl.deletePost);      // Supprimer un post
 
   app.use('/api/post', router);
 
